7113edc0404c347efc5452b3c41e238fb5f4803f
pranali <pranali.shetye@wohlig.com>
2019-04-08 18:31:35 +0530

    upload

---
+++ src/components/bookingheader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="head-top">
+
+  </div>  
+</template>
+
+
+<style lang="scss">
+@import "../assets/scss/booking/booking.scss";
+.head-top
+{
+    background-color: $headtop;
+    width:100%;
+    height:80px;
+}
+</style>
