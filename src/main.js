98851f1d3f97efa842850102beadb3037a7ed45d
prajaktakamble <prajakta.kamble@wohlig.com>
2019-04-09 09:59:28 +0530

    font awesome install

--- src/main.js
+++ src/main.js
@@ -1,16 +1,40 @@
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+// bootstrap install
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+// foontawesome install
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+import {
+  faCaretDown,
+  faTh,
+  faDumbbell
+} from "@fortawesome/free-solid-svg-icons"; //fas fa
 
+import {
+  faLinkedinIn,
+  faFacebookSquare,
+  faTwitter
+} from "@fortawesome/free-brands-svg-icons"; //fab
 
+import { faCommentAlt } from "@fortawesome/free-regular-svg-icons";
 
+library.add(faCaretDown, faTh, faDumbbell); //fas
+
+library.add(faLinkedinIn, faFacebookSquare, faTwitter); //fab
+
+library.add(faCommentAlt); //far
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
+
 new Vue({
   router,
   store,
