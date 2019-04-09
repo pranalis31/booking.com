5bf23ce8829e2dff8325e007416ab61c070c302d
pranali <pranali.shetye@wohlig.com>
2019-04-09 10:02:25 +0530

    file upload

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -5,10 +6,6 @@ import "./registerServiceWorker";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-
-
-
-
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 new Vue({
