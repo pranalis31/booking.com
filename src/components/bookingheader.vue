ea7e9a9a410927491ab45d0ec8b13afad9eb7ec5
prajaktakamble <prajakta.kamble@wohlig.com>
2019-04-09 12:58:39 +0530

    commit

--- src/components/bookingheader.vue
+++ src/components/bookingheader.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div class="head-top"></div>
 
   <div class="head-top">
@@ -8,31 +7,46 @@
         <b-col md="7" v-for="image in images" v-bind:key="image.index">
           <div class="img-display mt-3 text-left">
             <figure>
-              <b-img :src="image.img" class="img-size" alt="img1"/>
+              <b-img :src="image.img" class="img-size" alt="img1" />
             </figure>
           </div>
         </b-col>
         <b-col md="5">
           <div class="mt-3">
-            <font-awesome-icon class="text-light mr-3" :icon="['fas', 'rupee-sign']" /> 
-             <font-awesome-icon class="text-light mr-3" :icon="['fas', 'rupee-sign']" /> 
+            <font-awesome-icon
+              class="text-light mr-3"
+              :icon="['fas', 'rupee-sign']"
+            />
+            <font-awesome-icon
+              class="text-light mr-3"
+              :icon="['fas', 'rupee-sign']"
+            />
             <b-button
               variant="outline-light"
               size="sm"
               class="mr-3 btn text-light font-weight-bold"
-            >List Your Property</b-button>
-            <b-button variant="light" size="sm" class="text-dark mr-3">Register</b-button>
-            <b-button variant="light" size="sm" class="text-dark mr-3">Sign in</b-button>
+              >List Your Property</b-button
+            >
+            <b-button variant="light" size="sm" class="text-dark mr-3"
+              >Register</b-button
+            >
+            <b-button variant="light" size="sm" class="text-dark mr-3"
+              >Sign in</b-button
+            >
           </div>
         </b-col>
         <b-col md="12">
           <div class="mb-3">
             <b-navbar class="p-0">
-              <b-navbar-nav >
-                <b-nav-item href="#" class="mr-3 small">Accomodation</b-nav-item>
+              <b-navbar-nav>
+                <b-nav-item href="#" class="mr-3 small"
+                  >Accomodation</b-nav-item
+                >
                 <b-nav-item href="#" class="mr-3 small">Flights</b-nav-item>
                 <b-nav-item href="#" class="mr-3 small">Car rentals</b-nav-item>
-                <b-nav-item href="#" class="mr-3 small">Airport taxis</b-nav-item>
+                <b-nav-item href="#" class="mr-3 small"
+                  >Airport taxis</b-nav-item
+                >
               </b-navbar-nav>
             </b-navbar>
           </div>
@@ -40,7 +54,6 @@
       </b-row>
     </b-container>
   </div>
-
 </template>
 <script>
 import img1 from "../assets/images/logo.png";
@@ -58,11 +71,10 @@ export default {
 };
 </script>
 
-
 <style lang="scss">
 @import "../assets/scss/booking/booking.scss";
 .head-top {
-HEAD  background-color: $headtop;
+  headbackground-color: $headtop;
 
   width: 100%;
   height: 80px;
@@ -78,18 +90,14 @@ HEAD  background-color: $headtop;
   border-color: #f8f9fa;
   box-shadow: none;
 }
-.navbar-light{
-.navbar-nav {
-.nav-link 
-  {
-  color:#fff !important;
+.navbar-light {
+  .navbar-nav {
+    .nav-link {
+      color: #fff !important;
+    }
   }
 }
+.nav-item:hover {
+  background-color: $head-top-hover;
 }
-.nav-item:hover
-{
-  background-color:$head-top-hover;
-
-}
-
 </style>
