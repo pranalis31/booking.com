06159abfe94b1dfec996a17f9833ab59c1cb16e5
pranali <pranali.shetye@wohlig.com>
2019-04-11 16:22:06 +0530

    upload

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="footer-top">
+
+    </div>
+</template>
+
+<style lang="scss">
+@import "../assets/scss/booking/booking.scss";
+.footer-top
+{
+    width: 100%;
+    height:200px;
+    background-color: $headtop;
+  
+}
+</style>
