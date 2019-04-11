7e877aaf682b267483d769a9ad3fd9f8c48b8acd
pranali <pranali.shetye@wohlig.com>
2019-04-11 18:39:33 +0530

    upload

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -30,7 +30,7 @@
         <div class="text-center">
               <b-button variant="outline-light" size=sm class="mt-4 text-light ">List Your property</b-button>
         </div>
-        <div class="list mt-4 mmb-2"  >
+        <div class="list mt-4 mb-1"  >
             <span ><a  class="text-light small font-weight-bold border-bottom mr-3" href="">Table Version</a></span>
             <span ><a  class="text-light small font-weight-bold border-bottom mr-3" href="">Mobile Version</a></span>
             <span ><a  class="text-light small font-weight-bold border-bottom mr-3" href="">Your Account</a></span>
@@ -41,7 +41,78 @@
         </div>
             
     </div>
-    <div class="footer-links">
+    <div class="footer-links m-3">
+        <b-container>
+            <b-row>
+  <b-col md="2">
+      <div class="column text-left small">
+       <ul>
+        <li><a href="#">Countries</a></li>
+        <li><a href="#">Region</a></li>
+        <li><a href="#">Cities</a></li>
+        <li><a href="#">District</a></li>
+        <li><a href="#">Airport</a></li>
+        <li><a href="#">Hotels</a></li>
+        <li><a href="#">places of interest</a></li>
+       </ul>
+      </div>
+   </b-col>
+ <b-col md="2">
+      <div class="column text-left small">
+        <ul>
+        <li><a href="#">Holiday/rentals</a></li>
+        <li><a href="#">Apartments</a></li>
+        <li><a href="#">Resort</a></li>
+        <li><a href="#">Villas</a></li>
+        <li><a href="#">Hostels</a></li>
+        <li><a href="#">Beds</a></li>
+        <li><a href="#">Guest house</a></li>
+       </ul>
+      </div>
+  </b-col>
+  <b-col md="2">
+       <div class="column text-left small">
+        <ul>
+        <li><a href="#">Unique place to stay</a></li>
+        <li><a href="#">Reviwes</a></li>
+        <li><a href="#">Unpacked Travel articles</a></li>
+        <li><a href="#">Travel Communities</a></li>
+       
+       </ul>
+       </div>
+ </b-col>
+   <b-col md="2">
+        <div class="column text-left small">
+        <ul>
+        <li><a href="#">Car hire</a></li>
+        <li><a href="#">Flight Finder</a></li>
+        <li><a href="#">Restaurant resevation</a></li>
+        <li><a href="#">Booking.com for travel Agent</a></li>
+       
+       </ul>
+        </div>
+   </b-col>
+  <b-col md="2">
+       <div class="column text-left small">
+        <ul>
+        <li><a href="#">About booking.com</a></li>
+        <li><a href="#">Give website feedback</a></li>
+        <li><a href="#">Customer Service help</a></li>
+        <li><a href="#">Partners help</a></li>
+        <li><a href="#">Career</a></li>
+        <li><a href="#">Press Center</a></li>
+        <li><a href="#">Investigator relation</a></li>
+        <li><a href="#">Investigator relation</a></li>
+         <li><a href="#">Term and Condition</a></li>
+         <li><a href="#">Privacy and Cookie Staement</a></li>
+         <li><a href="#">Corporate Statement</a></li>
+       </ul>
+       </div>
+  </b-col>
+         </b-row>
+        </b-container>
+      
+      
         </div>
         
 </div> 
@@ -79,5 +150,8 @@
     margin-left: -17px;
     border: none;
     color: #fff;
+}
+.column ul li{
+list-style-type: none;
 }
 </style>
