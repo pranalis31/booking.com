f04194548912ee42c3116e4e6398ee59a6af36c5
pranali <pranali.shetye@wohlig.com>
2019-04-16 17:51:17 +0530

    upload

--- src/components/bookingline.vue
+++ src/components/bookingline.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="text text-left small mt-3 top-gude  font-weight-bold">
-    <font-awesome-icon class="text mr-1" :icon="['fas', 'star']" /> One of our
-    Top guide in Madrid
+    <span class="star-circle"
+      ><font-awesome-icon class="star1 text mr-1" :icon="['fas', 'star']" />
+    </span>
+    One of our Top guide in Madrid
   </div>
 </template>
 
@@ -10,5 +12,18 @@
 
 .top-guide {
   color: $color-brown;
+}
+.star-circle {
+  width: 15px;
+  height: 15px;
+  border: 1px solid $orange;
+  border-radius: 50%;
+  padding: 1px;
+  background-color: $orange;
+}
+.star1 {
+  color: $white-color;
+  margin: 2px;
+  font-size: 9px;
 }
 </style>
